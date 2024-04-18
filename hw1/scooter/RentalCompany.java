978d7016f703c654337ce798a132bd080c62ed3f
ambivert.t <151209994+tataios@users.noreply.github.com>
2024-04-18 17:52:37 +0400

    Oop_HMW1

---
+++ hw1/scooter/RentalCompany.java
@@ -0,0 +1,83 @@
+package homeworks.hw1.scooter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+
+public class RentalCompany {
+
+
+    private int uid = 0; // we will use this to id scooters
+
+    List<Scooter> scooters = new ArrayList<>(); // creates new arraylist scooters of type Scooter
+    private Scooter scooter;
+
+    // the list of all available scooters
+    public List<Scooter> availableScooters() {
+        List<Scooter> result = new ArrayList<>();
+        for (Scooter scooter: scooters) { //stands for operating over each scooter instance in scooters list
+            if (scooter.isAvailable()) {
+                result.add(scooter);
+            }
+        }
+        return result;
+    }
+
+    //adds scooter to the scooters list
+    public void addScooter(Scooter scooter) {
+        scooter.setId(uid++);
+        scooters.add(scooter);
+    }
+
+    //removes a scooter by id
+    public boolean removeScooter(int id) {
+        boolean removed = false;
+        for (Scooter scooter: scooters) {
+            if (scooter.getId() == id) {
+                scooters.remove(scooter);
+                removed = true;
+                break;
+            }
+        }
+        return removed;
+    }
+
+    // function to rent a scooter
+    public boolean rentScooter(int id) {
+        boolean rented = false;
+        for (Scooter scooter: scooters) {
+            if (scooter.getId() == id && scooter.isAvailable()) {
+                rented = true;
+                scooter.setAvailable = false;
+                break;
+            }
+        }
+        return rented;
+    }
+
+    // function to call when user returns a rented scooter
+    public boolean returnScooter(int id, double x, double y) {
+        for (Scooter scooter: scooters) {
+            if (scooter.getId() == id && scooter.isAvailable() == false) {
+                scooter.setAvailable = true;
+                scooter.setY(y);
+                scooter.setX(x);
+                break;
+            }
+        }
+        return scooter.isAvailable();
+    }
+    public void displayAllScooters() {
+        if (scooters.isEmpty()) {
+            System.out.println("No scooters available.");
+        } else {
+            System.out.println("List of all scooters:");
+            for (Scooter scooter : scooters) {
+                System.out.println(scooter.toString());
+            }
+        }
+    }
+}
+
+
---
+++ hw1/scooter/Scooter.java
@@ -0,0 +1,96 @@
+package homeworks.hw1.scooter;
+
+import java.util.Random;
+
+public class Scooter {
+
+
+
+    // Unique identifier for the scooter.
+    int id;
+
+    // Model name of the scooter (e.g., Sparrow X10).
+    String model;
+
+    // Current battery level (percentage).
+    int batteryLevel;
+
+    // Scooter location;
+    double x, y;
+
+    // Indicates if the scooter is available
+    boolean available;
+
+    public int getId() {
+        return id;
+    }
+
+    public boolean setAvailable;
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public int getBatteryLevel() {
+        return batteryLevel;
+    }
+
+    public void setBatteryLevel(int batteryLevel) {
+        this.batteryLevel = batteryLevel;
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+
+    public boolean isAvailable() {
+        return available;
+    }
+
+    public void setAvailable(boolean available) {
+        this.available = available;
+    }
+
+    /**
+     * Moves the scooter to the specified location.
+     * @param x The x coordinate of the new location.
+     * @param y The y coordinate of the new location.
+     */
+    public void travelTo(double x, double y) {
+        this.x = x;
+        this.y = y;
+        this.batteryLevel -= new Random().nextInt(15); // Consume some battery.
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("Scooter{");
+        sb.append("id=").append(id);
+        sb.append(", model='").append(model).append('\'');
+        sb.append(", batteryLevel=").append(batteryLevel);
+        sb.append(", x=").append(x);
+        sb.append(", y=").append(y);
+        sb.append(", available=").append(available);
+        sb.append('}');
+        return sb.toString();
+    }
+}
